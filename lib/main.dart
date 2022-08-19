239c826891bd098c25c0eef923d661ac0d966b8f
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-08-19 19:39:17 +0700

    update app
    cập nhật kết nối với firebase cho IOS, web, MacOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/page/home/bloc/todo_bloc.dart';
 import 'package:todo_app/page/home/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
